391408514cd8ea029b00047f50cd3ce0560281ce
angellalao <6369@holbertonstudents.com>
2023-06-06 06:08:29 -0700

    print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix == []:
-        return ""
-    for row in matrix:
+    for row in (matrix):
         for i in range(len(row)):
             if i == 0:
                 print("{:d}".format(row[i]), end="")
