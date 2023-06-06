47e2bbe964f17331b3ff307f508e73180a1b5688
angellalao <6369@holbertonstudents.com>
2023-06-06 06:10:06 -0700

    remove bracket, remove print space

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in (matrix):
+    for row in matrix:
         for i in range(len(row)):
             if i == 0:
                 print("{:d}".format(row[i]), end="")
             else:
                 print(" {:d}".format(row[i]), end="")
-        print(" ")
+        print("")
