6fb1878ee1ba116c1b063326b90aca4047c1a847
angellalao <6369@holbertonstudents.com>
2023-06-05 23:44:31 -0700

    add condition for matrix = 0

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if matrix == []:
+        return ""
     for row in matrix:
         for i in range(len(row)):
             if i == 0:
