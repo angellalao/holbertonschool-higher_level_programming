c4f0d6ebbfcaeee077683878c2b7bf4b0a34b05a
angellalao <6369@holbertonstudents.com>
2023-06-07 05:27:24 -0700

    use dictionary to remove duplicates

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    new_list = my_list.copy()
-    for element in new_list:
-        if new_list.count(element) > 1:
-            new_list.remove(element)
-    Sum = sum(new_list)
-    return Sum
+    new_list = list(dict.fromkeys(my_list))
+    return sum(new_list)
