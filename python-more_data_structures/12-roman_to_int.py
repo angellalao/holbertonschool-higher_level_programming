2ceaa9fb31e11a6707b0cf1e8d4ec0d389498eb9
angellalao <6369@holbertonstudents.com>
2023-06-09 00:38:50 -0700

    covert roman numerals to int

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    rom_num = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    reg_num = []
+    for letter in roman_string:
+        if letter in rom_num.keys():
+            reg_num.append(rom_num.get(letter))
+        x = 0
+        for num in range(len(reg_num) - 1):
+            if reg_num[x] < reg_num[x + 1]:
+                reg_num[x+1] = reg_num[x+1] - reg_num[x]
+                del reg_num[x]
+    return sum(reg_num)
