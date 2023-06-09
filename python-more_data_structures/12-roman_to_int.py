c9b6748212428709267e64639b4becf38a72ddc5
angellalao <6369@holbertonstudents.com>
2023-06-09 00:48:33 -0700

    make pycodestyle happy

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) == False or roman_string is None:
+    if isinstance(roman_string, str) is False or roman_string is None:
         return 0
-    rom_num = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     reg_num = []
     for letter in roman_string:
         if letter in rom_num.keys():
