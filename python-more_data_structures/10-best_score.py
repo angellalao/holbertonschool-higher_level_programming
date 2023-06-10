c2753008c5ad483df6aef331a5ed15a6d5dae542
angellalao <6369@holbertonstudents.com>
2023-06-09 21:25:05 -0700

    use max function

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    dictlist = list(a_dictionary.values())
-    dictlist.sort()
-    max_val = dictlist[-1]
-    for key, value in a_dictionary.items():
-        if value == max_val:
-            return key
+    if a_dictionary is not None:
+        return max(a_dictionary, key=a_dictionary.get)
+    return None
