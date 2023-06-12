fcd066023e49d19d09336f3fe31dea54818430ee
angellalao <6369@holbertonstudents.com>
2023-06-12 05:46:39 -0700

    create class square, instantiate with private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Write a class square that defines a square by
+private instance attribute: size
+"""
+
+
+class Square:
+    """ instantiate object with private attribute: size """
+    def __init__(self, size):
+        self.__size = size
