34b74fc94c2f8064b7ed5b3a25d5506894c1c687
angellalao <6369@holbertonstudents.com>
2023-06-12 17:43:02 -0700

    remove whitespaces

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,19 +6,19 @@
 class Square:
     """
     Defines class Square
-    
+
     Attributes:
        size (int): size of the square
     Methods:
        __init__(self, size=0): Initialises a new square instance
     """
     def __init__(self, size=0):
-        """ 
+        """
         Initialises a new Square Instance
-        
+
         Args:
         size (int): The size of the square. Default = 0
-        
+
         Raises:
         TypeError: If size is not an integer.
         ValueError: If size is less than 0.
