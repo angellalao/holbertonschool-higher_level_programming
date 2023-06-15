fc0d4edfeba22412a44c2ae4ae9e9b50800ca0dc
angellalao <6369@holbertonstudents.com>
2023-06-15 16:58:33 -0700

    add descriptions, cast float to int

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -19,16 +19,10 @@ def add_integer(a, b=98):
     Raises:
        TypeError: If a or b is not an int or float.
     """
-    if type(a) != int:
-        if type(a) == float:
-            a = int(a)
-        else:
-            raise TypeError("a must be an integer")
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
 
-    if type(b) != int:
-        if type(b) == float:
-            a = int(a)
-        else:
-            raise TypeError("b must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
 
-    return (a + b)
+    return int(a) + int(b)
