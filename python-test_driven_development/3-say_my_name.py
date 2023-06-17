17ff97bb8403fab88dca62283d0a887381417e2b
angellalao <6369@holbertonstudents.com>
2023-06-16 20:51:32 -0700

    add comments

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 
+"""
+ Contains function that prints string with first and last names as inputs
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Print a string  with the given first and last names.
+
+    Arguments:
+    - first_name: A string representing the first name.
+    - last_name: (Optional) String representing last name. Default is empty.
+
+    Returns:
+    - None
+
+    Raises:
+    - TypeError: If the first_name or last_name is not a string.
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
