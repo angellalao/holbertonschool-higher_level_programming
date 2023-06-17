240fb6f474ec3676c855db8e21f621de382cceb5
angellalao <6369@holbertonstudents.com>
2023-06-17 01:32:51 -0700

    add space before = sign

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -73,7 +73,7 @@ class Square:
         """
         Set new position
         """
-        error_msg= "position must be a tuple of 2 positive integers"
+        error_msg = "position must be a tuple of 2 positive integers"
         if type(value) != tuple:
             raise TypeError(error_msg)
         if len(value) != 2:
