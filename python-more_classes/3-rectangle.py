f67ab8f37b2dd1db940b0076c07e40cc89df06e1
angellalao <6369@holbertonstudents.com>
2023-06-18 01:58:44 -0700

    return empty string

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -112,7 +112,7 @@ class Rectangle:
             str: The string representation of the rectangle object.
         """
         if self.height == 0 or self.width == 0:
-            return ()
+            return ""
         rectangle = ""
         for i in range(self.height):
             for j in range(self.width):
