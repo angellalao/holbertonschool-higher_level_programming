dccaa8310855ec990835cc93d324f61fe023adec
angellalao <6369@holbertonstudents.com>
2023-06-20 05:45:03 -0700

    mylist

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+module with definition of MyList class
+"""
+
+
+class MyList(list):
+    """
+    print_sorted(self): method that prints and sorts list
+    """
+    def print_sorted(self):
+        """
+        sorts and prints given list
+        """
+        sorted_list = sorted(self)
+        print (sorted_list)
