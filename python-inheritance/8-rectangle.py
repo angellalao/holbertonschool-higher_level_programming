4d2ce352618ff81d8a70522e2c6b1abb6396dabc
angellalao <6369@holbertonstudents.com>
2023-06-21 05:00:01 -0700

    add description for init method

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -12,6 +12,9 @@ class Rectangle(BaseGeometry):
     integer_validator method inherited from parent class
     """
     def __init__(self, width, height):
+        """
+        Initialise subclass rectangle with width and height
+        """
         self.__width = width
         self.__height = height
         super().integer_validator("width", self.__width)
