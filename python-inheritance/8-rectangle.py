ed508679c440d59b909c638252b8096fb53909ae
angellalao <6369@holbertonstudents.com>
2023-06-21 04:20:38 -0700

    rectangle inherits method from parent class

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+class Rectangle created, which inherits from class BaseGeometry
+"""
+BaseGeometry = __import__(7-base_geometry).BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle = Subclass to BaseGeometry created.
+    Arguments width and height are validated through
+    integer_validator method inherited from parent class
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
