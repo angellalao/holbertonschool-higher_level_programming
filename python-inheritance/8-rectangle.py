fa8c93ac3687bbc0df89663273e0354490174ce6
angellalao <6369@holbertonstudents.com>
2023-06-21 04:57:25 -0700

    leave 2 spaces after module description

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
+BaseGeometry = __import__(7-base_geometry).BaseGeometry
 """
 class Rectangle created, which inherits from class BaseGeometry
 """
-BaseGeometry = __import__(7-base_geometry).BaseGeometry
 
 
 class Rectangle(BaseGeometry):
