984aff6e5b59c910849613cf719f3a6b0b3619e6
angellalao <6369@holbertonstudents.com>
2023-06-23 00:10:11 -0700

    convert to JSON

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import json
+"""
+This script defines a function to_json_string that converts an object
+into its JSON representation.
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Converts an object into its JSON representation.
+
+    Parameters:
+    - my_obj: The object to be converted.
+
+    Returns:
+    - A JSON string representing the object.
+
+    The function uses the json.dumps() method from the json module to
+    convert the object into its JSON representation.
+    The resulting JSON string is returned.
+    """
+    return json.dumps(my_obj)
