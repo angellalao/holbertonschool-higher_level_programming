b9718e60e644135203ecb1c54a2f92d991495350
angellalao <6369@holbertonstudents.com>
2023-06-24 18:29:48 -0700

    class to json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module contains function that returns a dictionary description
+of an object, for json representation
+"""
+def class_to_json(obj):
+    """
+    returns attributes of an object in the form
+    of a dictionary
+    """
+    dict_desc = vars(obj)
+    return dict_desc
