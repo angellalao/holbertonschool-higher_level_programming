44561b7d4b32e23f75bbdd98c70005a7a1df8c71
angellalao <6369@holbertonstudents.com>
2023-06-24 20:27:27 -0700

    reload attr from json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+module that creates a class Student
+"""
+
+
+class Student:
+    """class Student defined """
+    def __init__(self, first_name, last_name, age):
+        """
+        instantiate instance with first_name, last_name and age attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves the dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                new_dict[attr] = getattr(self, attr)
+        return new_dict
+
+    def reload_from_json(self, json):
+        """replaces attributes of student instance with attributes
+        from json file"""
+        for key, value in json.items():
+            setattr(self, key, value)
