61c5fee00bbe38c0f12ee263457914bece6467c5
angellalao <6369@holbertonstudents.com>
2023-06-24 18:32:35 -0700

    double blankline after description

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,6 +3,8 @@
 module contains function that returns a dictionary description
 of an object, for json representation
 """
+
+
 def class_to_json(obj):
     """
     returns attributes of an object in the form
