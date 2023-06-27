339a9e178f876a9721f3cd3b60e13299455b8389
angellalao <6369@holbertonstudents.com>
2023-06-26 21:46:20 -0700

    define rectangle class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Module description
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """defines class Rectangle, which inherits from class Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        self.width = value
+
+    @property
+    def height(self):
+        """retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.height = value
+
+    @property
+    def x(self):
+        """retrieve x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """set x"""
+        self.x = value
+
+    @property
+    def y(self):
+        """retrieve y"""
+        self.y = value
