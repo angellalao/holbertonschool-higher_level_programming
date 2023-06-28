c352b1837b7214cc131243fe1732df4d5b89b099
angellalao <6369@holbertonstudents.com>
2023-06-28 15:54:36 -0700

    class square, task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module that defines class square
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class square that inherits from class rectangle defined """
+    def __init__(self, size, x=0, y=0, id=None):
+        """instantiation of class """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """prints when print() or str() are invoked """
+        class_name = type(self).__name__
+        return f"[{class_name}] ({self.id}) {self.x}/{self.y}\
+ - {self.width}"
