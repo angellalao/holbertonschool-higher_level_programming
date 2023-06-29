190697d4b885ec65045f53195c4bdc82e9265e00
angellalao <6369@holbertonstudents.com>
2023-06-28 23:56:57 -0700

    move description

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -43,8 +43,8 @@ class Base:
             self.id = self.__nb_objects
 
     @staticmethod
-    """returns the JSON string represenation of a list of dictionaries"""
     def to_json_string(list_dictionaries):
+        """returns the JSON string represenation of a list of dictionaries"""
         if list_dictionaries:
             return json.dumps(list_dictionaries)
         else:
