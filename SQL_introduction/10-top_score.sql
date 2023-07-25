666e2472575b5c7e73dccbade92a5dee1f2f3eca
angellalao <6369@holbertonstudents.com>
2023-07-24 23:17:08 -0700

    show table with specific columns and order

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- a script that lists all records of the table second_table
+-- displays score and name(in this order)
+-- records should be ordered by score
+SELECT score, name FROM second_table
+ORDER BY score DESC;
