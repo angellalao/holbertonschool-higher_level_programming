9a6787dee192f38cd1eb34efa6b4c64cc0004eac
angellalao <6369@holbertonstudents.com>
2023-07-25 18:57:13 -0700

    double indent

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
 -- a script that lists all privileges of users  on 'localhost' server
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS
+	FOR 'user_0d_1'@'localhost';
+SHOW GRANTS
+	FOR 'user_0d_2'@'localhost';
