fe58c2cc1d952c8af85948f850ea8f83d9cd7306
angellalao <6369@holbertonstudents.com>
2023-07-30 19:17:41 -0700

    edit comment

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a script that lists all states from the database hbtn_0e_0_usa """
+"""a script that lists all states with name startign with 'N'
+from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
@@ -7,7 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id;")
+    cur.execute("SELECT * FROM states WHERE BINARY name \
+LIKE 'N%' ORDER BY id;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
