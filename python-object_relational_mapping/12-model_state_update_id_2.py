579907ae4b1dbc3687e76266b4e98033d3da0c52
angellalao <6369@holbertonstudents.com>
2023-07-31 22:32:07 -0700

    change name of object

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""a script that changes name of state object from the database"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+
+    session = Session()
+
+    state_id_to_edit = 2
+    state_to_edit = session.query(State).filter(State.id == state_id_to_edit)\
+                                        .first()
+    if state_to_edit:
+        state_to_edit.name = "New Mexico"
+    session.commit()
+    session.close()
