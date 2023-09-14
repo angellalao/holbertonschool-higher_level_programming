6e2db4ad0aa94d741b803893f56efaef9244d3f5
Angella Lao <6369@holbertonstudents.com>
2023-09-14 20:18:13 +1000

    change colour of header element, not red_header id

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -2,5 +2,6 @@ const header = document.querySelector("#red_header");
 header.onclick = changeColor;
 
 function changeColor() {
-    header.style.color = "#FF0000";
+    let headerElement = document.getElementsByTagName("header")[0];
+    headerElement.style.color = "#FF0000";
 }
