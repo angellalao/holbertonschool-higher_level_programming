13529a8c3288e056bf4f9a41e0f58a353097c886
Angella Lao <6369@holbertonstudents.com>
2023-09-19 01:22:37 +0000

    add space between function and bracket

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -2,7 +2,7 @@ $(function () {
   $.ajax({
     type: 'GET',
     url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
-    success: function(data) {
+    success: function (data) {
       $('div#character').text(data.name);
     }
   });
